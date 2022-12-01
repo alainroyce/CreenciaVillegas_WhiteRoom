6537965b56b0fb20c9e4b0997dc7771b8d7112b0
alainroyce <alain_creencia@dlsu.edu.ph>
2022-12-01 11:42:50 +0800

    Updated livingroom

---
+++ Assets/ArchVizPRO Interior Vol.5/3D SCRIPT/Bilboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bilboard : MonoBehaviour {
+
+
+	public Transform target;
+
+	void Start () {
+		target = GameObject.Find("Target_Billboard").transform;
+
+		Vector3 targetPostition  = new Vector3(target.position.x, this.transform.position.y, target.position.z);
+		transform.LookAt(targetPostition);
+		transform.Rotate(0, 180, 0);
+	}
+
+
+}
---
+++ Assets/ArchVizPRO Interior Vol.5/3D SCRIPT/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	public float rotationsPerMinute;
+
+	void Update () {
+		transform.Rotate(Vector3.forward * rotationsPerMinute * Time.deltaTime);
+	}
+}
